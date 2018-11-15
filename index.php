f328ff1eb299f67ae63d4d62366b1aae4d348923
Alan <alan_toro@hotmail.com>
2018-11-15 14:25:05 -0300

    Big Party App First Commit

---
+++ index.php
@@ -0,0 +1,317 @@
+<!doctype html>
+<html class="no-js" lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Brite</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">    
+        <link rel="stylesheet" href="css/animate.css">    
+        <!-- <link rel="stylesheet" href="css/bootstrap.min.css">     -->
+<!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="js/vegas/vegas.min.css">
+
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <nav class="main-nav navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="hamburger transition hamburger--efecto" type="button"
+                            aria-label="Menu" data-toggle="collapse" data-target=".navbar-collapse">
+                      <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                      </span>
+                    </button>                    
+                    <a class="navbar-brand transition" href="#">
+                        <!-- <h1><img class="img-responsive" src="img/estudioalegrialogo.png" alt="Estudio Alegría"></h1> -->
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#nosotros" data-toggle="collapse" data-target=".navbar-collapse">Nosotros</a></li>
+                        <li >
+                            <a href="#servicios" data-toggle="collapse" data-target=".navbar-collapse">servicios</a>
+                         </li>
+                        <li ><a href="#profesionales" data-toggle="collapse" data-target=".navbar-collapse">galeria</a></li>
+                        <li ><a href="#contacto" data-toggle="collapse" data-target=".navbar-collapse">contacto</a></li>
+                        <li ><a href="#contacto" data-toggle="collapse" data-target=".navbar-collapse">novedades</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header>
+            <a href="javascript:;">
+                <img src="img/logo.png" alt="Big Party" class="img-responsive wow fadeIn animated">
+            </a>
+        </header>
+        <nav class="text-center nav-index  wow fadeIn animated">
+            <ul class="list-inline">
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#servicios">servicios</a></li>
+                <li><a href="#profesionales">galeria</a></li>
+                <li><a href="#contacto">contacto</a></li>
+                <li><a href="#contacto">novedades</a></li>
+            </ul>
+        </nav>
+        <section  id="banner-inicio">  
+             
+        </section> 
+        <section id="nosotros" class="bg-2 content" >
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-4 text-center">
+                        <h2>NOSOTROS</h2>
+                    </div>  
+                    <div class="col-md-6 col-sm-8">
+                        <P class="paragraph-1">Big Party es un lugar único con dos amplios salones desarrollados en 1.200 m2. Exclusivamente ambientados para todo tipo de eventos. Contamos con equipamientos y instalaciones de última generación, coordinado por un equipo altamente profesional. Nuestro objetivo principal es superar todas las expectativas de los invitados y en especial, la del homenajeado en ese día tan anhelado. Para ello brindamos un cálido servicio de excelencia y un perfecto estado de mantenimiento de los espacios.</P>
+                    </div>  
+                </div>
+            </div>
+        </section>
+        <section id="banner-2" class="content text-center">
+            <a href="javascript:;">
+                <img src="img/logo2.png" alt="Big Party" class="img-responsive wow fadeIn animated">
+            </a>
+
+        </section>
+        <section id="soc-emp" class="bg-img  wow fadeIn animated text-center" style="background-image: url(img/recurso-2.png);">
+            <h1 class="color-1">SOCIAL / EMPRESARIAL</h1>
+            
+        </section>
+        <section id="soc-emp-data" class="bg-4 content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <p class="paragraph-1 color-1">Te ofrecemos nuestras instalaciones para vivir esos momentos tan especiales junto a la familia y amigos. Brindamos servicios de excelencia y te ofrecemos un variado sistema de catering. Con nosotros podrás vivenciar las fiestas más inolvidables y las reuniones más placenteras junto a tus seres queridos. Amplias posibilidades de organización y variadas propuestas adaptadas a cada gusto personal.</p>
+                    </div>  
+                    <div class="col-md-6 col-sm-6">
+                        <p class="paragraph-1 color-1">Contamos con gran experiencia en ambientaciones para todos los estilos, servicios de DJ profesional con iluminación de última generación, shows exclusivos de primer nivel y propuestas variadas para cada evento</p>
+                    </div>  
+                </div>
+            </div>            
+        </section>
+        <secion id="servicios">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 content text-center">
+                        <h2>NUESTROS SERVICIOS</h2>
+                        <ul class="list-inline">
+                            <li><a href="javascript:;">Bautismos /</a></li>
+                            <li><a href="javascript:;">Comuniones /</a></li>
+                            <li><a href="javascript:;">Quince Años /</a></li>
+                            <li><a href="javascript:;">Bar Mitzva /</a></li>
+                            <li><a href="javascript:;">Casamientos /</a></li>
+                            <li><a href="javascript:;">Adultos /</a></li>
+                            <li><a href="javascript:;">Teens (Disco Party) /</a></li>
+                            <li><a href="javascript:;">Corporativos Empresas /</a></li>
+                            <li><a href="javascript:;">Fiestas temáticas /</a></li>
+                            <li><a href="javascript:;">Fiestas de egresados /</a></li>
+                            <li><a href="javascript:;">Espacios para cortos publiciatrios /</a></li>
+                            <li><a href="javascript:;">TV /</a></li>
+                            <li><a href="javascript:;">Cine</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Espacios para cortos publiciatrios
+                        </span>
+                        
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Fiestas de Egresados
+                        </span>
+                        
+                    </a>                       
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+                        
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+                        
+                    </a>                       
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+                        
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+                        
+                    </a>                       
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                        <span class="salon_name transition"> 
+                            Martin Sigal
+                        </span>
+
+                    </a>                       
+                </div>
+            </div>
+        </secion>
+        <section id="infantiles">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 text-center content">
+                        <a href="javascript:;">
+                            <img src="img/logo.png" alt="Big Party" class="img-responsive wow fadeIn animated">
+                        </a>                    
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="banner-infantiles fullwidth bg-img" style="background-image: url('img/banner-infantiles.jpg');">
+                        
+                    </div>
+                    <div class="content bg-3 fullwidth text-center">
+                        <div class="col-xs-12 col-sm-8 col-md-6 col-lg-5">
+                            <p class="paragraph-1 color-1">Hacemos de cada cumple momentos mágicos para cada chico, brindando la posibilidad de compartir con amigos los juegos más divertidos y las propuestas más originales. Hacemos realidad tu fiesta soñada</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="salon-section">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="salon-section__title col-xs-12 col-sm-8 col-md-6 col-lg-5 text-center">
+                        <h2>SALON BIG 1</h2>
+                        <p class="paragraph-1">En el salón de Planta Baja contamos con 600 m2 para organizar un evento único e irrepetible. Muy luminoso, con un amplio sector de juegos que cuenta con tobogán inflable gigante, laberinto, tobogán con pelotero, tirolesa, cama elástica y más! Además cuenta con mini disco, sala blanda, metegol y disfraces para niños de hasta 6 o 7 años. Un muy confortable espacio social integrado para los adultos con muebles de estilo moderno, minimalista hasta el último detalle que lo hace exclusivo en zona norte y Gran Buenos Aires</p>
+                    </div>
+                    <div class="clearfix"></div>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                       
+                    </a>
+                    <a href="javascript:," class="salon" style="background-image: url('img/martin-sigal.jpg');">
+                        <span class="salon_overlay transition"></span>
+                       
+                    </a>
+                    <div class="clearfix"></div>
+                </div>
+                
+            </div>
+        </section>
+        <footer class="bg-1 fullsize">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <h3 class="color-3 wow fadeIn animated" data-wow-duration="1000ms" data-wow-delay="100ms"><a href="mailto:RESERVAS@BIGPARTY.COM.AR">RESERVAS@BIGPARTY.COM.AR</a></h3>
+                        
+                        <ul class="redes list-inline wow fadeIn animated" data-wow-duration="1000ms" data-wow-delay="100ms">
+                            <li>
+                                <a href="javascript:;"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="javascript:;"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p class="paragraph-1">cuyo 1981 - martinez</p>
+                <a class="paragraph-1" href="tel:01147333800">011 4733 3800 / </a>
+                <a class="paragraph-1" href="tel:47333400">4733 3400</a>
+                
+            </div>
+            
+        </footer>       
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<!--         <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script> -->
+        <script src="js/plugins.js"></script>
+        <script src="js/wow.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="js/main.js"></script>
+        <script src="js/vegas/vegas.min.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("#banner-inicio").vegas({
+                    delay: 7000,
+                    transition: 'blur',
+                    transitionDuration: 1000,
+                    firstTransitionDuration: 3000,
+                    slides: [
+                        { src: "img/recurso-1.png" },
+                        { src: "img/recurso-2.png" }
+                    ]
+                });
+                $("#soc-emp").vegas({
+                    delay: 9000,
+                    animation: 'kenburnsUp',
+                    transition: 'fade',
+                    overlay: true,
+                    timer: false,
+                    transitionDuration: 1000,
+                    firstTransitionDuration: 3000,
+                    slides: [
+                        { src: "img/recurso-1.png" },
+                        { src: "img/recurso-2.png" }
+                    ]
+                });
+
+            });
+        </script>
+    </body>
+</html>
